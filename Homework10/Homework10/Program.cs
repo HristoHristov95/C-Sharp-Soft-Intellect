c563fb3ef4390df0f82c5aa3ce8274241819a738
HristoHristov95 <hristo_ultimeted@yahoo.com>
2016-01-18 22:51:15 +0200

    newhomework

---
+++ Homework10/Homework10/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Homework10
+{
+    class Program
+    {
+        public static String readfile(String fileName)
+        {
+            String content;
+             using(StreamReader rd=new StreamReader(fileName))
+             {
+                 content = rd.ReadToEnd();
+             }
+             return content;
+        }
+        public static void writeFile(String filename,String content)
+        {
+            filename = filename + ".txt";
+            using(StreamWriter wr=new StreamWriter(filename))
+            {
+                wr.Write(content);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter filename to read : ");
+            String filename=Console.ReadLine();
+            try
+            {
+               String content=readfile(filename);
+                Console.WriteLine("Enter filename to write file : ");
+                String fileNameWrite=Console.ReadLine();
+                writeFile(fileNameWrite, content);
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(ArgumentException e){
+                Console.WriteLine(e.Message);
+            }
+            catch(Exception e){
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Homework10/Zadacha2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Zadacha2
+{
+    class Program
+    {
+        public static void readAndCheck (String fileName)
+        {
+            fileName=fileName+".txt";
+            using(StreamReader rf=new StreamReader(fileName))
+            {
+                while(!rf.EndOfStream)
+                {
+                    String temp = rf.ReadLine();
+                    if(temp.Contains('.') || temp.Contains('?') || temp.Contains('!'))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        throw new SentenceNotCompleted("The sentence was not complete !");
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter file to read : ");
+            String filename = Console.ReadLine();
+            try
+            {
+                readAndCheck(filename);
+            }catch(SentenceNotCompleted e){
+                Console.WriteLine(e.Message);
+                return;
+           
+            }catch(IOException e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("This is a default exception !");
+                return;
+            }
+            Console.WriteLine("Everything was okey in the file .");
+        }
+    }
+}
